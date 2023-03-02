694c521e73e34424a7e47355c0a887abc13e914b
Dinesh Kumar <dinesh78088@gmail.com>
2023-03-02 11:28:28 +0530

    few more condition added

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ let userAge = prompt('what is your age')
 document.querySelector('#age').innerText = userAge
 
 
-if(userAge > 18){
+if(userAge <= 10){
 
-    document.querySelector('.result').innerText = 'You are adult, you can drink Coke'
+    document.querySelector('.result').innerText = 'You can drink Milk'
 
-} else {
+} else if(userAge <= 18){
 
-    document.querySelector('.result').innerText = 'You are child, you can drink Milk'
+    document.querySelector('.result').innerText = 'You can drink Coke'
+
+} else if(userAge <= 22){
+
+    document.querySelector('.result').innerText = 'You can drink Beer'
+
+} else{
+
+    document.querySelector('.result').innerText = 'You can drink Wine'
 
 }
